8d25fade3b3fc05c9788240bc4eacc9a77da1333
Alex Borian <aboryan24@gmail.com>
2025-01-06 09:22:19 -0600

    fix: add repo necessary to build test image, follow copr release schema

--- build-kmod-it87-extras.sh
+++ build-kmod-it87-extras.sh
@@ -6,8 +6,13 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
+if [[ "${RELEASE}" -ge 41 ]]; then
+    COPR_RELEASE="rawhide"
+else
+    COPR_RELEASE="${RELEASE}"
+fi
 
-curl -LsSf -o /etc/yum.repos.d/_copr_grandpares-it87-extras.repo "https://copr.fedorainfracloud.org/coprs/grandpares/it87-extras/repo/fedora-${RELEASE}/grandpares-it87-extras--fedora-${RELEASE}.repo"
+curl -LsSf -o /etc/yum.repos.d/_copr_grandpares-it87-extras.repo "https://copr.fedorainfracloud.org/coprs/grandpares/it87-extras/repo/fedora-${COPR_RELEASE}/grandpares-it87-extras--fedora-${COPR_RELEASE}.repo"
 
 ### BUILD it87-extras (succeed or fail-fast with debug output)
 dnf install -y \
